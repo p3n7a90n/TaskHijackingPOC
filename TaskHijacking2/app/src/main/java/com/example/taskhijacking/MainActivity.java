49351018447af66f50845c5848316b00a418edbf
Sanjay Das <sd84700@gmail.com>
2024-01-18 09:55:54 +0530

    Initial Commit

---
+++ TaskHijacking2/app/src/main/java/com/example/taskhijacking/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.taskhijacking;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.taskhijacking.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
